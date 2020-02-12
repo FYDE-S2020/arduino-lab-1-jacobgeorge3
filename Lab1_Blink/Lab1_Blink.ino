31ef1a9a064914f8180c9a2a6a6c8fcb5ee425af
jacobgeorge3 <jacob.c.george333@gmail.com>
2020-02-12 13:18:25 -0600

    Wrote Dimmer Function

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,9 +1,10 @@
 #define LED_PIN 2
+#define LED_BUILTIN 2
 
 void setup() {
   // put your setup code here, to run once:
   // initialize digital pin LED_PIN as an output.
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void timedBlink(int interval) {
@@ -13,10 +14,21 @@ void timedBlink(int interval) {
   delay(interval);                       // wait for a second
 }
 
+void dimmer(int freq, int duty) {
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(offTime);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 250; i <= 1000; i+=i){
-    timedBlink(i);
+  for(int i = 100; i >= 0; i--){
+    dimmer(50 , i);
     }
   
 }
